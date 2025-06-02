75b1b791f12db2303f71ef8b5e811d9b40ae4286
Prajwal-18MD <prajju.18gryphon@gmail.com>
2025-06-02 20:41:17 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { FaInstagram, FaXTwitter, FaLinkedin } from "react-icons/fa6";
+import { FaInstagram, FaXTwitter, FaLinkedin, FaGithub } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/huzaifahmedz/"
+        href="https://www.instagram.com/_.praj.wal._/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://x.com/PrajwalMD18"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <FaXTwitter size={24} />
       </a>
       <a
-        href="https://www.linkedin.com/in/huzaif-ahmed-6abb49273/"
+        href="https://www.linkedin.com/in/prajwal-m-d"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaLinkedin size={24} />
+      </a>
+      <a
+        href="https://github.com/Prajwal18-MD"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub size={24} />
       </a>
     </footer>
   );
