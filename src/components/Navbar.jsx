777c94f31dcabcfd4ef5f61fca2eda187a9c050d
Prajwal-18MD <prajju.18gryphon@gmail.com>
2025-06-16 23:23:27 +0530

    Bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { menu, close } from "../assets";
+import logo from "../assets/luffy.png";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -42,7 +43,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src="src\assets\luffy.png" alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Prajwal &nbsp;
             <span className='sm:block hidden'> | Devnity</span>
